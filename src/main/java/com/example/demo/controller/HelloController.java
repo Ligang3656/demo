0f2ed3a68c4bdfb46d8b00f64535f31d1c47cf35
ligang <1364886288@qq.com>
2022-11-14 16:36:52 +0800

    first Commit

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description
+ * @author: ligang
+ * @date: 2022/11/04/ 20:22
+ **/
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String Hello(@RequestParam String name){
+        return ("HelloWorld,你好世界"+name);
+    }
+    @PostMapping("/study")
+    public String study(@RequestParam String book){
+        return ("HelloWorld,来读这本"+book);
+    }
+}
